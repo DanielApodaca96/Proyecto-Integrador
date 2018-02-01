f6a425dc9e148d092133fcf1b8ef1d936e79cf66
Unknown <Lorenzomagic21@gmail.com>
2018-01-31 18:21:36 -0700

    cambie de carpeta
    nueva carpeta htdocs

---
+++ js/animaciones.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#login").fadeIn(1000);
+  $(".logindiv").fadeIn(1000);
+
+  $(".btn1").mouseover(function(){
+    $(".btn1").css("background-size", "50px");
+});
+});
