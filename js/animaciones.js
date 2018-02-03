1098a7dadfdba293a600fd960048682ed56d87f5
Unknown <Lorenzomagic21@gmail.com>
2018-02-03 12:56:45 -0700

    Seccion contacto
    se modifico el contacto y las animaciones del js

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,8 +1,31 @@
+var bandera=true;
+var altoDiv;
 $(document).ready(function () {
   $("#login").fadeIn(1000);
   $(".logindiv").fadeIn(1000);
+  altoDiv=$("#encabezado").height();
 
-  $(".btn1").mouseover(function(){
-    $(".btn1").css("background-size", "50px");
-});
+  $(window).scroll(function(){
+      var posicion=$(window).scrollTop();
+      if ($("#encabezado").height()>posicion) {
+        var promedio=((posicion*100)/altoDiv)/100;
+        $("#nav").css({
+          "background-color":"rgba(10,10,10,"+promedio+")"
+        });
+    }else{
+          $("#nav").css({
+            "background-color":"rgba(10,10,10,1)",
+            "height":"50px"
+          }),
+          $(".navbar").css({
+            "padding-top":"0",
+            "padding-botton":"0",
+            "border-radius":"0"
+          }),
+          $("#imglogo").css({
+            "width":"35px",
+            "padding-top":"5px"
+          });
+    }
+  });
 });
