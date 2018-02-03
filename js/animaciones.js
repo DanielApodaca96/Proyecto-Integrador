7ef4ca7389b5e0f8525ba3562b9657bf92e6c0a7
Unknown <dapodacafilms@gmail.com>
2018-02-03 15:02:57 -0700

    Logo completo, inicio de panel admin y foto de inicio
    Varios pequenos cambios

--- js/animaciones.js
+++ js/animaciones.js
@@ -7,12 +7,12 @@ $(document).ready(function () {
 
   $(window).scroll(function(){
       var posicion=$(window).scrollTop();
-      if ($("#encabezado").height()>posicion) {
+      /*if ($("#encabezado").height()>posicion) {
         var promedio=((posicion*100)/altoDiv)/100;
         $("#nav").css({
           "background-color":"rgba(10,10,10,"+promedio+")"
         });
-    }else{
+      }else{
           $("#nav").css({
             "background-color":"rgba(10,10,10,1)",
             "height":"50px"
@@ -23,9 +23,29 @@ $(document).ready(function () {
             "border-radius":"0"
           }),
           $("#imglogo").css({
-            "width":"35px",
+            "height":"45px",
             "padding-top":"5px"
           });
-    }
+      }*/
+      if ($("#encabezado").height()>posicion) {
+        var promedio=((posicion*100)/altoDiv)/100;
+        $("#nav").css({
+          "background-color":"rgba(10,10,10,"+promedio+")"
+        });
+      }else{
+          $("#nav").css({
+            "background-color":"rgba(10,10,10,1)",
+            "height":"50px"
+          }),
+          $(".navbar").css({
+            "padding-top":"0",
+            "padding-botton":"0",
+            "border-radius":"0"
+          }),
+          $("#imglogo").css({
+            "height":"45px",
+            "padding-top":"5px"
+          });
+      }
   });
 });
