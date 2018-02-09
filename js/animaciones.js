4c71f7676bf808b5240f56c69e449a55c7a498ad
Unknown <Lorenzomagic21@gmail.com>
2018-02-08 20:36:20 -0700

    Neva seccion logros
    nueva seccion y modificacion de animaciones

--- js/animaciones.js
+++ js/animaciones.js
@@ -4,6 +4,26 @@ $(document).ready(function () {
   $("#login").fadeIn(1000);
   $(".logindiv").fadeIn(1000);
   altoDiv=$("#myCarousel").height();
+  var posicion=$(window).scrollTop();
+      var promedio=((posicion*100)/altoDiv)/100;
+
+  if(promedio>=0.1){
+    $("#nav").animate({
+      height: '50px',
+      opacity: '2px'
+    }),
+    $(".navbar").css({
+      "padding-top":"0",
+      "padding-botton":"0",
+      "border-radius":"0"
+    }),
+    $("#imglogo").animate({
+      height:'45px'
+    }),
+    $("#nav").css({
+      "background-color":"rgba( 33,37,41,"+promedio+")"
+    });
+  }
 
   $(window).scroll(function(){
       var posicion=$(window).scrollTop();
