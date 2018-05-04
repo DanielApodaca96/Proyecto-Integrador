2f605930d5c4a6cad76b780c655ec108ac9120e3
Daniel Apodaca <dapodacafilms@gmail.com>
2018-05-03 18:59:57 -0600

    PDF script

--- Oneami/resources/views/reportes/reporte.blade.php
+++ Oneami/resources/views/reportes/reporte.blade.php
@@ -17,24 +17,36 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
   </head>
   <body>
-    <div class="row">
-      <div class="col-md-2 col-md-offset-2 col-xs-4 ">
-        <a class="navbar-brand" >
-            <img id="midLogo" src="{{ asset('img/icons/OneamiLogoBlack.svg') }}" alt="">
-        </a>
+    <div id="todo">
+      <div class="row">
+        <div class="col-md-2 col-md-offset-2 col-xs-4 ">
+          <a class="navbar-brand" >
+              <img id="midLogo" src="{{ asset('img/icons/OneamiLogoBlack.svg') }}" alt="">
+          </a>
+        </div>
+        <div class="col-md-4 col-xs-6 col-md-offset-2 col-xs-offset-2">
+          <h2 class="color-gray align-right">Lista de Alumnos.</h2>
+          <h5 class="fecha color-gray align-right">Fecha {{ date('Y-m-d h:m:s') }}</h5>
+        </div>
       </div>
-      <div class="col-md-4 col-xs-6 col-md-offset-2 col-xs-offset-2">
-        <h2 class="color-gray align-right">Lista de Alumnos.</h2>
-        <h5 class="fecha color-gray align-right">Fecha {{ date('Y-m-d h:m:s') }}</h5>
-      </div>
+      <hr>
     </div>
-    <hr>
-
-
   </body>
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <!-- Include all compiled plugins (below), or include individual files as needed -->
   <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+  <script type="text/javascript">
+    function imprimir(elemento){
+      var respaldo = $('body').html();
+      var div = $('#'+elemento).clone();
+      $('body').empty().html(div);
+      window.print();
+      $('body').html(respaldo);
+    }
+    $(document).ready(function(){
+      imprimir('todo');
+    });
+  </script>
 
 </html>
